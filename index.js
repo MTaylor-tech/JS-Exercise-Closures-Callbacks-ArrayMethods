b840625eb940bc258a6cec6708d057f6007f7f32
Matthew Taylor <matt@mtaylor.tech>
2020-01-04 21:18:32 -0500

    processDuplicateFree

--- index.js
+++ index.js
@@ -156,8 +156,14 @@ function processContains(item, list, callback) {
  * [2] Invoking `processDuplicateFree` passing `[1,1,2,2,3]` and `(arr) => arr.length`,
  * should return 3.
 */
-function processDuplicateFree(/* CODE HERE ONLY AFTER COMPLETING ALL OTHER TASKS */) {
-  /* CODE HERE ONLY AFTER COMPLETING ALL OTHER TASKS */
+function processDuplicateFree(list, callback) {
+  let uniques = [];
+  for (let i=0; i<list.length; i++) {
+    if (!uniques.includes(list[i])) {
+        uniques.push(list[i]);
+    }
+  }
+  return callback(uniques);
 }
 
 /////////////// HIGHER-ORDER ARRAY METHODS ///////////////
